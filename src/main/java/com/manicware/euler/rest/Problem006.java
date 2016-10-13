d3b0361140456a0aaf420d2a43633e9843e20475
William Witt <william@witt-family.net>
2016-10-12 21:52:34 -0500

    Correcting typos wiht Problem 6

--- src/main/java/com/manicware/euler/rest/Problem006.java
+++ src/main/java/com/manicware/euler/rest/Problem006.java
@@ -32,7 +32,7 @@ public class Problem006 implements Problem {
             "1^2 + 2^2 + ... + 10^2 = 385\n" +
             "The square of the sum of the first ten natural numbers is,\n" +
             "\n" +
-            "(1 + 2 + ... + 10)^2 = 552 = 3025\n" +
+            "(1 + 2 + ... + 10)^2 = 55^2 = 3025\n" +
             "Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.\n" +
             "\n" +
             "Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.\n" +
